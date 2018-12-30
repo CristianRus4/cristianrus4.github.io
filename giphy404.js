f5d6b2a8eab19a29d4ed4793d8e9f73e0bd0d2dc
Cristian Rus <cristianrus4@gmail.com>
2018-12-30 11:02:35 +0100

    goats

--- giphy404.js
+++ giphy404.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-	q = "404 error";
+	q = "funny goat";
 	
 	request = new XMLHttpRequest;
 	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
