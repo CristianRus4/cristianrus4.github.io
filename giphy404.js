3544d80f26f832eeb9c3b24d3086d3a59680fbfb
Cristian Rus <cristianrus4@gmail.com>
2018-12-30 10:59:58 +0100

    y

--- giphy404.js
+++ giphy404.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-	q = "404";
+	q = "404 error";
 	
 	request = new XMLHttpRequest;
 	request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
